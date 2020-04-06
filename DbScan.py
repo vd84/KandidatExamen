8663e0138bbee540ef9e9f1c7cac479cdb4274eb
douglashammarstam <dogge20@hotmail.com>
2020-04-06 11:07:47 +0200

    dataset

--- DbScan.py
+++ DbScan.py
@@ -22,18 +22,22 @@ def run(X, labels_true, experiment_number, eps, minPts, samples):
     db = DBSCAN(eps=eps, min_samples=minPts).fit(X)
     skl_labels = db.labels_
 
+
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 
     # Plot results of scikit DBSCAN
 
     # Black removed and is used for noise instead.
     unique_labels = set(skl_labels)
+    clusters_found = len(unique_labels)
     colors = [plt.cm.Spectral(each)
               for each in np.linspace(0, 1, len(unique_labels))]
     for k, col in zip(unique_labels, colors):
         if k == -1:
             # Black used for noise.
             col = [0, 0, 0, 1]
+            clusters_found -= 1
+
 
         class_member_mask = (skl_labels == k)
 
@@ -44,7 +48,8 @@ def run(X, labels_true, experiment_number, eps, minPts, samples):
         xy = X[class_member_mask & ~core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
                  markeredgecolor='k', markersize=6)
-    plot_text = "DBSCAN PLOT, experiment number: "  + str(experiment_number) + " Samplesize = " + str(samples) + "\nminPts=" + str(minPts)
+    plot_text = "DBSCAN PLOT, experiment number: "  + str(experiment_number) + " Samplesize = " + \
+                str(samples) + "\nminPts=" + str(minPts) + " Clusters found = " + str(clusters_found)
     plt.title(plot_text)
     plt.show()
 
